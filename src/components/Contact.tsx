ff567515151d717118a8b07f54405517eb85cc0d
Tasnimul Hassan <124423618+TasnimulHassan02@users.noreply.github.com>
2025-04-19 11:27:43 +0600

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -34,15 +34,16 @@ const Contact: React.FC = () => {
               <div className="space-y-6">
                 <div className="flex items-center">
                   <Phone className="mr-4" size={20} />
-                  <p>(123) 456-7890</p>
+                  <p>+8801987660586</p>
                 </div>
                 <div className="flex items-center">
                   <Mail className="mr-4" size={20} />
-                  <p>contact@impactz.com</p>
+                  <p>impactz2025@gmail.com</p>
                 </div>
                 <div className="flex items-center">
                   <MapPin className="mr-4" size={20} />
-                  <p>123 Innovation Drive, Tech City</p>
+                  <p>TA-107, Gulshan 1
+                    Dhaka, Bangladesh</p>
                 </div>
               </div>
               
@@ -58,7 +59,7 @@ const Contact: React.FC = () => {
                       <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.166 6.839 9.489.5.09.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.343-3.369-1.343-.454-1.155-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.337-.012 2.416-.012 2.744 0 .267.18.578.688.48C19.137 20.164 22 16.42 22 12c0-5.523-4.477-10-10-10z"></path>
                     </svg>
                   </a>
-                  <a href="#" className="bg-white/20 hover:bg-white/40 transition-colors p-2 rounded-full">
+                  <a href="https://www.linkedin.com/company/impactz2025/?viewAsMember=true" className="bg-white/20 hover:bg-white/40 transition-colors p-2 rounded-full">
                     <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                       <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
                     </svg>
